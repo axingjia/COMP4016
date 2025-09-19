b22ee4ffb11fd131b979d0ef9355a730705cce1c
Alan Huang <axingjia@gmail.com>
2025-09-19 11:32:11 -0700

    .

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const PORT = 8080;
 
 app.use(bodyParser.json());
 
-// 1. Simple GET request: /foo -> bar
+
 app.get("/foo", (req, res) => {
   res.send("bar");
 });
 
-// 2. POST request: /hello -> Hello {name}!
+
 app.post("/hello", (req, res) => {
   const name = req.body.name || "World";
   res.send(`Hello ${name}!`);
 });
 
-// 3. GET request: /kill -> stop server
+
 app.get("/kill", (req, res) => {
   res.send("Shutting down...");
   console.log("Server shutting down...");
